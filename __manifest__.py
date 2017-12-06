bccd77baba24e220d70744467122d82fe15085d5
James Dominy <james@fribergfineart.com>
2017-12-06 09:20:20 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Child Theme Clarico',
+    'category' : 'Website',
+    'version' : '11.0.0.1',
+    'summary': 'Modifications for Clarico Theme',
+    'description': 'Modifications for Clarico Theme',
+    'depends': [
+            'website',     
+            'theme_clarico',
+    ],
+    'author': 'Creative Fine Art, James Dominy',
+    'website': 'http://www.LuNelerp.com',
+    'maintainer': 'James Dominy',
+    'installable': True,
+}
